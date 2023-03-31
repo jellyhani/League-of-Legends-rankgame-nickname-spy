ca8e3c22fdb8e706ef22e66b401f3d1cc57d2752
jellyhani <62514874+jellyhani@users.noreply.github.com>
2023-04-01 08:50:31 +0900

    Update league.py

--- league.py
+++ league.py
@@ -5,11 +5,13 @@ import requests
 import urllib.parse
 import urllib3
 import time
-import os
+import os 
 from bs4 import BeautifulSoup
+import ctypes
 
 process_name = 'LeagueClientUx.exe'
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+ctypes.windll.kernel32.SetConsoleTitleW("opgg_multisearch")
 
 def check_process():
     while True:
@@ -17,14 +19,10 @@ def check_process():
             output = subprocess.check_output(f'tasklist /fi "imagename eq {process_name}"', shell=True).decode('iso-8859-1')
             if process_name in output:
                 print(f"{process_name} process found!")
-                command = 'wmic PROCESS WHERE name=\'LeagueClientUx.exe\' GET commandline'
+                command = f'wmic PROCESS WHERE name=\'{process_name}\' GET commandline'
                 output = subprocess.check_output(command, shell=True).decode('utf-8')
                 tokens = ["--riotclient-auth-token=", "--riotclient-app-port=", "--remoting-auth-token=", "--app-port=", "--region="]
-                riot_port = None
-                riot_token = None
-                client_port = None
-                client_token = None
-                region = None
+                riot_port, riot_token, client_port, client_token, region = [next((token[len(token):-1] for token in tokens if token in output), None) for _ in range(5)]
 
                 for token in tokens:
                     value = output.split(token)[1].split()[0].strip('"')
@@ -39,24 +37,28 @@ def check_process():
                         riot_token = value
                     if token == "--region=":
                         region = value
-
                 riot_api = f'https://riot:{riot_token}@127.0.0.1:{riot_port}'
                 client_api = f'https://riot:{client_token}@127.0.0.1:{client_port}'
-                time.sleep(1)
+
                 lobby(client_port, region, riot_api, client_api)
             else:
-                print(f"{process_name} process not found. Retrying in 5 seconds...", end="\r")
+                print(f"{process_name} process not found. Retrying...", end="\r")
         except subprocess.CalledProcessError:
-            print(f"Error: {process_name} process not found. Retrying in 5 seconds...", end="\r")
-        time.sleep(5)
+            print(f"Error: {process_name} process not found. Retrying...", end="\r")
         os.system('cls' if os.name=='nt' else 'clear')
 
 def lobby(client_port, region, riot_api, client_api):
     search_performed = False
+    time.sleep(10)
     while True:
         lobby_check = requests.get(riot_api + '/lol-gameflow/v1/gameflow-phase', verify=False)
         lobby_check_json = json.loads(lobby_check.text)
         if lobby_check_json == 'ChampSelect':
+            text = lobby_check.text
+            formatted_text = f"{text:<18}"
+            print(formatted_text.replace('"', ''), end="\r")
+            cleared_text = formatted_text[:len(text)]
+            print(cleared_text.replace('"', ''), end="\r")
             if not search_performed:
                 if client_port:
                     url = client_api + '/chat/v5/participants/champ-select'
@@ -119,16 +121,10 @@ def lobby(client_port, region, riot_api, client_api):
                                 opgg_url = f"https://op.gg/multisearch/{region}?summoners=" + urllib.parse.quote(",".join(names))
                                 webbrowser.open(opgg_url)
                                 search_performed = True
-                                text = lobby_check.text
-                                formatted_text = f"{text:<18}"
-                                print(formatted_text.replace('"', ''), end="\r")
-                                cleared_text = formatted_text[:len(text)]
-                                print(cleared_text.replace('"', ''), end="\r")    
                     else:
                         print("Error:", response.status_code)     
         else:
             search_performed = False
-            time.sleep(5)
             output = subprocess.check_output(f'tasklist /fi "imagename eq {process_name}"', shell=True).decode('iso-8859-1')
             if process_name in output:
                 text = lobby_check.text
@@ -138,7 +134,7 @@ def lobby(client_port, region, riot_api, client_api):
                 print(cleared_text.replace('"', ''), end="\r")
             else:
                 os.system('cls' if os.name=='nt' else 'clear')
-                print(f"{process_name} process not found. Retrying in 5 seconds...", end="\r")
+                print(f"{process_name} process not found. Retrying...", end="\r")
                 check_process()
 
 check_process()
